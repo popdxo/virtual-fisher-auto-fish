4c787eb3e234b33975c4d85f81ce94edbd9f5e43
brick <63986294+popdxo@users.noreply.github.com>
2024-10-02 23:54:48 -0400

    Update run.js

--- run.js
+++ run.js
@@ -3,7 +3,12 @@ const { spawn } = require('child_process');
 const autoFish = spawn('node', ['autoFish.js']);
 const antiDetection = spawn('node', ['antiDetection.js']);
 
-autoFish.stdout;
-antiDetection.stdout;
+autoFish.stdout.on('close', (code) => {
+    console.log(`autofish killed itself again. crazy. ${code}`);
+    startAutoFish();
+});
+antiDetection.stdout.on('data', (data) =>
+    console.log(`${data}`)
+);
 
 //i copied and pasted this because im i have no clue what im doing
